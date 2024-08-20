cf1e404ab2a26a363c770577fe1c2d342ba8bff4
aisaka-1653 <reobata1127@gmail.com>
2024-08-20 23:12:49 +0900

    feat: login画面への導線を実装

--- src/components/templates/CenteredContentLayout.tsx
+++ src/components/templates/CenteredContentLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { UserAuthFooter } from "../orgranisms/form/UsersAuthFooter";
 
 export const CenteredContentLayout = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="container mx-auto px-4 max-w-md">
         <Outlet />
+        <UserAuthFooter />
       </div>
     </div>
   );
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,3 +1,4 @@
+import { Login } from "@/components/pages/Login";
 import { Signup } from "@/components/pages/Signup";
 import { CenteredContentLayout } from "@/components/templates/CenteredContentLayout";
 import { Route, Routes } from "react-router-dom";
@@ -7,6 +8,7 @@ export const Router = () => {
     <Routes>
       <Route element={<CenteredContentLayout />}>
         <Route path="users/signup" element={<Signup />} />
+        <Route path="users/login" element={<Login />} />
       </Route>
     </Routes>
   );

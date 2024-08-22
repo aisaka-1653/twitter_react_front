149b6adc6070d0487cad6e72b1d8e63472e510fb
aisaka-1653 <reobata1127@gmail.com>
2024-08-22 23:08:46 +0900

    chore: sonnerコンポーネントをインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/theme-provider";
 import { Router } from "./router/Router";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <Router />
+        <Toaster />
       </BrowserRouter>
     </ThemeProvider>
   );

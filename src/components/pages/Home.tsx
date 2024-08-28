b0b0e21f39b8b8823c48555bf578f1ef7615453c
aisaka-1653 <reobata1127@gmail.com>
2024-08-28 22:18:55 +0900

    feat: Homeページを仮実装

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,11 @@
+import apiClient from "@/apis/apiClient";
+import { Button } from "../ui/button";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2>ツイート一覧画面</h2>
+      <Button onClick={() => apiClient.get("/tweets")}>test</Button>
+    </div>
+  );
 };

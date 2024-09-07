6d4d169b0b6005114487eb31eafbce72b028f75a
aisaka-1653 <reobata1127@gmail.com>
2024-09-07 21:48:54 +0900

    feat: ツイートが画像のみであればtransformでcontentに空文字を挿入

--- src/utils/schema/tweetFormSchema.ts
+++ src/utils/schema/tweetFormSchema.ts
@@ -4,7 +4,11 @@ const IMAGE_TYPES = ["image/jpeg", "image/png"];
 
 export const tweetFormSchema = z
   .object({
-    content: z.string().max(140, "140文字以内で入力してください").optional(),
+    content: z
+      .string()
+      .max(140, "140文字以内で入力してください")
+      .optional()
+      .transform((val) => (val === undefined ? "" : val)),
     image: z
       .custom<File>()
       .refine((image) => image && IMAGE_TYPES.includes(image.type), {

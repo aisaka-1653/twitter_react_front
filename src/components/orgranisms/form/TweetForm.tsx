b7df7b5f2fed0473645bea1e1342b379d57ab176
aisaka-1653 <reobata1127@gmail.com>
2024-09-07 22:38:51 +0900

    feat: TweetFormのテキスト入力部品をコンポーネント化

--- src/components/orgranisms/form/TweetForm.tsx
+++ src/components/orgranisms/form/TweetForm.tsx
@@ -1,4 +1,3 @@
-import { AutoResizeTextArea } from "@/components/atoms/AutoResizeTextArea";
 import { UserAvatar } from "@/components/atoms/UserAvatar";
 import { TweetFormFooter } from "@/components/molecules/TweetFormFooter";
 import { FormProvider, useForm } from "react-hook-form";
@@ -7,55 +6,36 @@ import {
   TweetFormSchema,
 } from "@/utils/schema/tweetFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import { FC } from "react";
+import { TweetFormContent } from "@/components/molecules/TweetFormContent";
 
 type TweetFormProps = {
   placeholder: string;
-  onSubmit: (tweet: TweetFormSchema) => void;
+  onSubmit: (tweet: TweetFormSchema) => Promise<void>;
 };
 
-export const TweetForm: FC<TweetFormProps> = (props) => {
-  const { placeholder, onSubmit } = props;
+export const TweetForm: FC<TweetFormProps> = ({ placeholder, onSubmit }) => {
   const form = useForm<TweetFormSchema>({
     resolver: zodResolver(tweetFormSchema),
-    defaultValues: {
-      content: "",
-    },
+    defaultValues: { content: "" },
   });
+
+  const handleSubmit = async (data: TweetFormSchema) => {
+    await onSubmit(data);
+    form.reset();
+  };
 
   return (
     <FormProvider {...form}>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
           <div className="flex max-w-[598px] px-4 gap-2">
             <div className="pt-1">
               <UserAvatar url="https://github.com/shadcn.png" />
             </div>
             <div className="flex flex-col size-full max-w-[518px] gap-3 pt-2">
-              <div className="p-0.5">
-                <FormField
-                  control={form.control}
-                  name="content"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <AutoResizeTextArea
-                          {...field}
-                          placeholder={placeholder}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+              <TweetFormContent placeholder={placeholder} />
               <TweetFormFooter />
             </div>
           </div>

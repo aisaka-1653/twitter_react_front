e2b0e17cfeae73fce27969b6a1f87a391c5a8f45
aisaka-1653 <reobata1127@gmail.com>
2024-09-07 15:38:02 +0900

    fix: TextareaAutoSizeコンポーネントがラップしているtextareaにforwardRefでrefを適用

--- src/components/atoms/AutoResizeTextArea.tsx
+++ src/components/atoms/AutoResizeTextArea.tsx
@@ -1,13 +1,22 @@
-import TextareaAutosize from "react-textarea-autosize";
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from "react-textarea-autosize";
 import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
 
-export const AutoResizeTextArea = ({ ...props }) => {
+type AutoResizeTextAreaProps = TextareaAutosizeProps & { placeholder: string };
+
+export const AutoResizeTextArea = forwardRef<
+  HTMLTextAreaElement,
+  AutoResizeTextAreaProps
+>(({ ...props }, ref) => {
   return (
     <TextareaAutosize
       className={cn(
         "overflow-hidden resize-none size-full bg-background text-xl focus-visible:outline-none"
       )}
       {...props}
+      ref={ref}
     />
   );
-};
+});

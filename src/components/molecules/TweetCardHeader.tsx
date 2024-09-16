ab2ac838341655695da2105ee9c7fe6d71159ef5
aisaka-1653 <reobata1127@gmail.com>
2024-09-16 22:03:28 +0900

    feat: TweetCardHeaderを実装

---
+++ src/components/molecules/TweetCardHeader.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { TweetMoreDropdown } from "../orgranisms/TweetMoreDropdown";
+import { User } from "@/types/user";
+import { FC } from "react";
+
+type TweetUserInfoProps = {
+  user: User;
+};
+
+export const TweetCardHeader: FC<TweetUserInfoProps> = ({ user }) => {
+  return (
+    <div className="flex justify-between items-center gap-1 text-sm">
+      <div>
+        <Link to={`/users/${user.id}`} className="font-bold">
+          {user.display_name}
+        </Link>
+        <span className="text-muted-foreground ml-1">
+          {`@${user.username}`}
+        </span>
+      </div>
+      <TweetMoreDropdown user={user} />
+    </div>
+  );
+};

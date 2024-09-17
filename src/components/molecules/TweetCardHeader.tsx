d87277c93883aa1e610324bf333e858c0329aa14
aisaka-1653 <reobata1127@gmail.com>
2024-09-17 13:39:12 +0900

    style: ユーザー表示名をhover時にunderlineを追加

--- src/components/molecules/TweetCardHeader.tsx
+++ src/components/molecules/TweetCardHeader.tsx
@@ -11,7 +11,7 @@ export const TweetCardHeader: FC<TweetUserInfoProps> = ({ user }) => {
   return (
     <div className="flex justify-between items-center gap-1 text-sm">
       <div>
-        <Link to={`/users/${user.id}`} className="font-bold">
+        <Link to={`/users/${user.id}`} className="font-bold hover:underline">
           {user.display_name}
         </Link>
         <span className="text-muted-foreground ml-1">

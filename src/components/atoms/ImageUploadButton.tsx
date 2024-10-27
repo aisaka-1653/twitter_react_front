41d600d98eff54ac4993e1780172ec056811b2db
aisaka-1653 <reobata1127@gmail.com>
2024-10-27 20:41:07 +0900

    refactor: ImageUploadButtonコンポーネントの再利用性を拡張

--- src/components/atoms/ImageUploadButton.tsx
+++ src/components/atoms/ImageUploadButton.tsx
@@ -1,16 +1,20 @@
-import { FC, useRef } from "react";
-import { Image } from "lucide-react";
+import { FC, ReactNode, useRef } from "react";
 import { Button } from "../ui/button";
 import { UseFormRegister } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
 
 type ImageUploadButtonProps = {
   register: UseFormRegister<any>;
   onFileChange: (file: File) => void;
+  className?: string;
+  children: ReactNode;
 };
 
 export const ImageUploadButton: FC<ImageUploadButtonProps> = ({
   register,
   onFileChange,
+  className,
+  children,
 }) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -39,10 +43,10 @@ export const ImageUploadButton: FC<ImageUploadButtonProps> = ({
         type="button"
         variant="circle"
         size="icon"
-        className="text-blue-300"
+        className={twMerge("", className)}
         onClick={handleImageClick}
       >
-        <Image className="h-5 w-5" />
+        {children}
       </Button>
     </>
   );

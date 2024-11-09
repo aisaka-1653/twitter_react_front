06c57e586d3827c1299b7ab13da835b58900970a
aisaka-1653 <reobata1127@gmail.com>
2024-11-10 01:14:19 +0900

    feat: ツイートに対するコメント数を表示

--- src/components/molecules/TweetCardFooter.tsx
+++ src/components/molecules/TweetCardFooter.tsx
@@ -1,28 +1,27 @@
 import { Bookmark, Heart, Repeat } from "lucide-react";
-import { TweetCardFooterIcon } from "./TweetCardFooterIcon";
 import { CommentForm } from "../orgranisms/form/CommentForm";
 import { Tweet } from "@/types/tweet";
+import { TweetCardFooterButton } from "./TweetCardFooterButton";
 
 type TweetCardFooterProps = {
   tweet: Tweet;
 };
 
 export const TweetCardFooter: React.FC<TweetCardFooterProps> = ({ tweet }) => {
+  const iconSize = "w-4";
+
   return (
     <div className="w-full flex justify-between">
       <CommentForm tweet={tweet} />
-      <TweetCardFooterIcon
-        className="hover:bg-green-500/20 hover:text-green-400"
-        icon={<Repeat />}
-      />
-      <TweetCardFooterIcon
-        className="hover:bg-rose-500/20 hover:text-rose-400"
-        icon={<Heart />}
-      />
-      <TweetCardFooterIcon
-        className="hover:bg-amber-500/20 hover:text-amber-400"
-        icon={<Bookmark />}
-      />
+      <TweetCardFooterButton className="hover:bg-green-500/5 hover:text-green-400">
+        <Repeat className={iconSize} />
+      </TweetCardFooterButton>
+      <TweetCardFooterButton className="hover:bg-rose-500/5 hover:text-rose-400">
+        <Heart className={iconSize} />
+      </TweetCardFooterButton>
+      <TweetCardFooterButton className="hover:bg-amber-500/5 hover:text-amber-400">
+        <Bookmark className={iconSize} />
+      </TweetCardFooterButton>
     </div>
   );
 };

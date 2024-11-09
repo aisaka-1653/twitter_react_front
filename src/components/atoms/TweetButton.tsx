8bc51422c7b3007ab24e05e2543dfb232be189f7
aisaka-1653 <reobata1127@gmail.com>
2024-11-09 18:31:26 +0900

    feat: TweetButtonのカスタム性を拡張

--- src/components/atoms/TweetButton.tsx
+++ src/components/atoms/TweetButton.tsx
@@ -1,21 +1,27 @@
 import { FC, ReactNode } from "react";
 import { Button } from "../ui/button";
+import { twMerge } from "tailwind-merge";
 
 type TweetButtonProps = {
   isDisabled: boolean;
   onClick?: () => void;
+  className?: string;
   children: ReactNode;
 };
 
 export const TweetButton: FC<TweetButtonProps> = ({
   isDisabled,
   onClick,
+  className,
   children,
 }) => (
   <Button
     type={onClick ? "button" : "submit"}
     size="full"
-    className="bg-sky-500 text-white font-bold rounded-full hover:bg-sky-500/90"
+    className={twMerge(
+      "bg-sky-500 text-white font-bold rounded-full hover:bg-sky-500/90",
+      className,
+    )}
     disabled={isDisabled}
     onClick={onClick}
   >

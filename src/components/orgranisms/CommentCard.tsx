9d9cea2b043912c1886bed790ba848fff40f54f2
aisaka-1653 <reobata1127@gmail.com>
2024-11-10 14:51:55 +0900

    fix: contentがcardからはみ出る問題を修正

--- src/components/orgranisms/CommentCard.tsx
+++ src/components/orgranisms/CommentCard.tsx
@@ -15,18 +15,16 @@ export const CommentCard: React.FC<CommentCardProps> = ({ comment }) => {
       <CardContent className="p-4 py-3">
         <div className="flex gap-2">
           <UserAvatar user={user} />
-          <div className="flex flex-col size-full">
-            <div className="flex justify-between items-center gap-1 text-sm">
-              <div>
-                <Link to={`/users/${user.id}`}>
-                  <span className="font-bold hover:underline">
-                    {user.display_name}
-                  </span>
-                </Link>
-                <span className="text-muted-foreground ml-1">
-                  {`@${user.username}`}
+          <div className="flex flex-col min-w-0 flex-1">
+            <div className="flex items-center gap-1 text-sm">
+              <Link to={`/users/${user.id}`}>
+                <span className="font-bold hover:underline">
+                  {user.display_name}
                 </span>
-              </div>
+              </Link>
+              <span className="text-muted-foreground ml-1">
+                {`@${user.username}`}
+              </span>
             </div>
             <p className="whitespace-pre text-wrap break-words">{content}</p>
           </div>
